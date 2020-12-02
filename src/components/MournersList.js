d0171c8a299747a00610597440e9c609eaa02501
MiriamPeskowitz <miriam5000@gmail.com>
2020-12-02 18:11:16 -0500

    moved all files into components

--- src/components/MournersList.js
+++ src/components/MournersList.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const MournersList = ( {users} ) => {
 	console.log("users:", users)
+	
 	const names = users.map((user) => (
 		<span key={user.id}> {user.attributes.name} </span>
 	))
