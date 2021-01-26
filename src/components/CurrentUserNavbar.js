d26a3c1fc94df165f7743dace98f6f4f9ff38a20
MiriamPeskowitz <miriam5000@gmail.com>
2021-01-25 22:05:14 -0500

    Show page works

--- src/components/CurrentUserNavbar.js
+++ src/components/CurrentUserNavbar.js
@@ -1,26 +1,20 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-//Decision: pass currentUser as prop, or do mapstatetoProps? 
 
 const gray = {color: '#C0C0C0'};
 
 const CurrentUserNavbar = ( {currentUser} ) => {	
-	// console.log("CurrentUserNavbar/user: ", currentUser.username)
-	// const user = currentUser.data.attributes.username
 	const user = currentUser.username
-
 	return (
-	  <ul className="current-user-nav-bar">
+	  <ul className="nav-bar current-user-nav-bar">
 			<li> Welcome Back, {user} </li>
 	 		<li> <NavLink style={gray} exact to="/">Home</NavLink> </li> 
 	 		<li> <NavLink style={gray} exact to="/profile">Profile</NavLink> </li> 
 	    <li> <NavLink style={gray} exact to="/logout">Logout</NavLink> </li> 			    
 	  </ul> 
-
-	  
-	   )
-	}
+	)
+}
 
 export default CurrentUserNavbar
 					   
--- src/components/NewSessionNavbar.js
+++ src/components/NewSessionNavbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+
 const gray = {color: '#C0C0C0'};
 
 const NewSessionNavbar = () => {	
 	return (
-		<ul>
+		<ul className="nav-bar current-user-nav-bar">
 	 		<li> <NavLink style={gray} exact to="/">Home</NavLink> </li> 
 	 		<li> <NavLink style={gray} exact to="/signup">SignUp</NavLink> </li>    
 	    <li> <NavLink style={gray} exact to="/login">Login</NavLink> </li>	   
