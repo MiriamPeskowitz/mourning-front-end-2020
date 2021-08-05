a2f836c1e4a62ead7f5012c7133045d6f3300d0a
MiriamPeskowitz <miriam5000@gmail.com>
2021-08-05 12:13:55 -0400

    Tidy PEC component after changes

--- src/components/ProfileEntryCards.js
+++ src/components/ProfileEntryCards.js
@@ -4,14 +4,13 @@ import { NavLink } from 'react-router-dom'
 
 const ProfileEntryCards = ( {entries} ) => {
 	// console.log("PEC, entries:", entries)
-
 	const ent = entries.reverse()
 
 	const individualEntries = ent.map(entry => (
 			<p>
 				<li key={entry.id}>
-					<NavLink to={`/entries/${entry.id}`} >
-					{entry.title}:  {entry.content} 
+					<NavLink to={`/entries/${entry.id}`}>
+						{entry.title}:  {entry.content} 
 					</NavLink>
 				</li>
 			</p>
@@ -20,37 +19,17 @@ const ProfileEntryCards = ( {entries} ) => {
 				
 	const myEntryCards = ent.length > 0 ? individualEntries : <p>No entries</p>
 
-
 	return <ul className="profile-list">{myEntryCards}</ul>
-
 }
 
 const mapStateToProps = state => {
 	return ({
 		entries: state.authReducer.currentUser.entries
-		// cu: state.authReducer.currentUser
-	
-
 	})
 }
 	
 export default connect(mapStateToProps, null )(ProfileEntryCards)
 					// activeStyle={{color: "white"}}>
 
-// entryiesList format
-// const EntriesList = ( {entries} ) => {
-	
-// 	const feelings = entries.map((entry) => (
-	
-// 		<li key={entry.id}> 
-// 			{entry.attributes.title}: {entry.attributes.content} 
-// 		</li>
-		
-// 	))
-// 	return (
-// 		<div className="entries-list">
-// 			<ul>{feelings}</ul>
-// 		</div>
-// 	)
 // }
 // export default EntriesList
