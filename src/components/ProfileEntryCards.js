8a82e568a47a9d0d2aea7b4d7fbe8d9fce02d5f3
MiriamPeskowitz <miriam5000@gmail.com>
2021-08-04 21:59:34 -0400

    Refactor for consistency in list display, esp profileEntryCards.js

--- src/components/ProfileEntryCards.js
+++ src/components/ProfileEntryCards.js
@@ -1,31 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom' 
-// import Card from 'react-bootstrap/Card'
 
-// send as props, or mapStateToProps
-//went with props passed from Profile. Why? Is there an async thing that was happening with MSP? But why would this be solved with passed props? Should I delete MSP? 
-//other ways to solve something like this? 
-//because that hack didn't work
-//now back to msp, still doesn't work coming straight from login 
-
-//Why did I have withRouter? since I'm not passing history. Delete? 
 const ProfileEntryCards = ( {entries} ) => {
 	// console.log("PEC, entries:", entries)
 
 	const ent = entries.reverse()
 
-	const myEntryCards = ent.length > 0 ? ent.map(entry => (<li key={entry.id}><NavLink to={`/entries/${entry.id}`} activeStyle={{color: "white"}}>{entry.title}--{entry.content} </NavLink></li>)) : <p>No entries</p>
+	const individualEntries = ent.map(entry => (
+				<li key={entry.id}>
+					<NavLink to={`/entries/${entry.id}`} 
+					activeStyle={{color: "white"}}>
+					{entry.title}:  {entry.content} 
+					</NavLink><br/>
+				</li>
+			))
+				
+	const myEntryCards = ent.length > 0 ? individualEntries : <p>No entries</p>
 
+//change to li -- 
+	return <ul className="profile-entry-cards">{myEntryCards}</ul>
 
-	return (	
-		<>
-			<div className="profile-entry-cards">
-				{ myEntryCards }
-			</div>	
-			<br/>
-		</>
-	)
 }
 
 const mapStateToProps = state => {
@@ -39,4 +34,20 @@ const mapStateToProps = state => {
 	
 export default connect(mapStateToProps, null )(ProfileEntryCards)
 
-
+// entryiesList format
+// const EntriesList = ( {entries} ) => {
+	
+// 	const feelings = entries.map((entry) => (
+	
+// 		<li key={entry.id}> 
+// 			{entry.attributes.title}: {entry.attributes.content} 
+// 		</li>
+		
+// 	))
+// 	return (
+// 		<div className="entries-list">
+// 			<ul>{feelings}</ul>
+// 		</div>
+// 	)
+// }
+// export default EntriesList
